fe2ce37e7b2d19bb6fc75818404b1fba8bfa1a26
Kamikaze0420 <bic191@live.com>
2023-01-16 23:34:59 -0800

    Update on_env_start.bat

--- scripts/on_env_start.bat
+++ scripts/on_env_start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-@echo. & echo "Stable Diffusion UI - v2" & echo.
+@echo. & echo "Stable Diffusion UI - v4" & echo.
 
 set PATH=C:\Windows\System32;%PATH%
 
@@ -41,7 +41,7 @@ if "%update_branch%"=="" (
     @echo. & echo "Downloading Stable Diffusion UI.." & echo.
     @echo "Using the %update_branch% channel" & echo.
 
-    @call git clone -b "%update_branch%" https://github.com/cmdr2/stable-diffusion-ui.git sd-ui-files && (
+    @call git clone -b "%update_branch%" https://github.com/Kamikaze-420/stable-diffusion-ui.git sd-ui-files && (
         @echo sd_ui_git_cloned >> scripts\install_status.txt
     ) || (
         @echo "Error downloading Stable Diffusion UI. Sorry about that, please try to:" & echo "  1. Run this installer again." & echo "  2. If that doesn't fix it, please try the common troubleshooting steps at https://github.com/cmdr2/stable-diffusion-ui/wiki/Troubleshooting" & echo "  3. If those steps don't help, please copy *all* the error messages in this window, and ask the community at https://discord.com/invite/u9yhsFmEkB" & echo "  4. If that doesn't solve the problem, please file an issue at https://github.com/cmdr2/stable-diffusion-ui/issues" & echo "Thanks!"
@@ -50,11 +50,10 @@ if "%update_branch%"=="" (
     )
 )
 
-@xcopy sd-ui-files\ui ui /s /i /Y /q
-@copy sd-ui-files\scripts\on_sd_start.bat scripts\ /Y
-@copy sd-ui-files\scripts\bootstrap.bat scripts\ /Y
-@copy sd-ui-files\scripts\check_modules.py scripts\ /Y
-@copy "sd-ui-files\scripts\Start Stable Diffusion UI.cmd" . /Y
+@REM @xcopy sd-ui-files\ui ui /s /i /Y /q
+@REM @copy sd-ui-files\scripts\on_sd_start.bat scripts\ /Y
+REM @copy sd-ui-files\scripts\bootstrap.bat scripts\ /Y
+@REM @copy "sd-ui-files\scripts\Start Stable Diffusion UI.cmd" . /Y
 @copy "sd-ui-files\scripts\Developer Console.cmd" . /Y
 
 @call scripts\on_sd_start.bat
